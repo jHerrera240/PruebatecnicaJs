73bfdc2b20d4190a3efe4ffbe78e8a6b3af6e290
Jose Herrera <hjose0650@gmail.com>
2022-03-06 14:27:19 -0600

    PT060322- Conectamos el producto con el formulario

--- javaS.js
+++ javaS.js
@@ -1,18 +1,31 @@
-/*1. Crear un objeto producto. */
+/*1. Crear un objeto product. */
 
-let productos = {
-    nombre: 'manzana',
-    categoria: 'Frutas',
-    precio: 1.99
+const products = {
+    name: "product",
+    category:"category",
+    price: 000
+}
+console.log(products)
+
+let apple = Object.assign({},products)
+apple.name = 'apple'
+apple.category = 'Frutas'
+apple.price = 2000
+console.log(apple, products)
+
+function search(){
+    var productname = document.getElementById("product").value
+    console.log(productname)
+    var productprice = parseInt(document.getElementById("price").value)
+    if (productname == apple.name || productprice == apple.price )
+        alert("We find the fruit :)")
+    else
+        alert("Erroooooooooooooooor")
 }
 
-console.log(productos)
 
-function Añadir (){
 
-}
-
-/*Crear una función que permita añadir un producto o varios en un 
-array, la  función debe permitir añadir el código del producto, 
-nombre, color y peso. 3. Crear una función 
-que permita consultar el total de productos que hay en el  array. */
+/*Crear una función que permita añadir un product o varios en un 
+array, la  función debe permitir añadir el código del product, 
+name, color y peso. 3. Crear una función 
+que permita consultar el total de products que hay en el  array. */
