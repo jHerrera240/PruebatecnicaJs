e5be7c6d578b0f19855445d08a0e2f31bd745e04
Jose Herrera <hjose0650@gmail.com>
2022-03-06 11:17:53 -0600

    PT060322- Iniciamos la prueba tecnica

---
+++ javaS.js
@@ -0,0 +1,18 @@
+/*1. Crear un objeto producto. */
+
+let productos = {
+    nombre: 'manzana',
+    categoria: 'Frutas',
+    precio: 1.99
+}
+
+console.log(productos)
+
+function Añadir (){
+
+}
+
+/*Crear una función que permita añadir un producto o varios en un 
+array, la  función debe permitir añadir el código del producto, 
+nombre, color y peso. 3. Crear una función 
+que permita consultar el total de productos que hay en el  array. */
