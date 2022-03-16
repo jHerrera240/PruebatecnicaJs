fe1cefe3993ceac04776f80b7953e8768a84d5fd
Jose Herrera <hjose0650@gmail.com>
2022-03-15 19:52:19 -0600

    Se cambió el nombre 'ob1' por un nombre que tenga que ver con lo programado

--- javaS.js
+++ javaS.js
@@ -2,68 +2,73 @@
 
 const products = {
     name: "product",
-    category:"category",
+    category: "category",
     price: 000
 }
 console.log(products)
-let obj1 =  Object.assign({},products)
-let apple = Object.assign({},products)
-    apple.name = 'apple'
-    apple.category = 'Fruits'
-    apple.price = 2000
-    console.log(apple, products)
-
-function search(){
+let watermelon = Object.assign({}, products)
+let apple = Object.assign({}, products)
+apple.name = 'apple'
+apple.category = 'Fruits'
+apple.price = 2000
+console.log(apple, products)
+// let productoArray = products[
+//     {
+//         "name": "watermelon",
+//         "category" : "fruit",
+//         "price": 12000
+//     },
+    
+// ]
+function search() {
     var productName = document.getElementById("product").value
-        console.log(productName)
+    console.log(productName)
     var productPrice = parseInt(document.getElementById("price").value)
-    if (productName == apple.name || productPrice == apple.price )
+    if (productName == apple.name || productPrice == apple.price)
         alert("We find the fruit :)")
     else
         alert("Erroooooooooooooooor")
 }
-
-function create(){
+function create() {
     console.log("Creating the article")
     var productName = document.getElementById("product").value
-        console.log(productName)
+    console.log(productName)
     var productPrice = parseInt(document.getElementById("price").value)
-        console.log(productPrice)
-    var productCategory =document.getElementById("category").value
-        console.log(productCategory)
-    if(productName == "" & productCategory == "" &  productPrice == "" )
+    console.log(productPrice)
+    var productCategory = document.getElementById("category").value
+    console.log(productCategory)
+    if (productName == "" & productCategory == "" & productPrice == "")
         alert("Errooooor")
     else
-        obj1.name = productName
-        obj1.category = productCategory
-        obj1.price = productPrice
-
-    return obj1
+        watermelon.name = productName
+        watermelon.category = productCategory
+        watermelon.price = productPrice
+    return watermelon
 }
 
 
-function removing(){
+function removing() {
     console.log("removing the article")
     var productName = document.getElementById("product").value
-        console.log(productName)
+    console.log(productName)
     var productPrice = parseInt(document.getElementById("price").value)
-        console.log(productPrice)
-    var productCategory =document.getElementById("category").value
-        console.log(productCategory)
-    if (productName == apple.name & productCategory == apple.category){
+    console.log(productPrice)
+    var productCategory = document.getElementById("category").value
+    console.log(productCategory)
+    if (productName == apple.name & productCategory == apple.category) {
         alert("Borrando la manzana")
         delete apple.name
         delete apple.category
         delete apple.price
         console.log(apple)
     }
-    else if (productName == obj1.name  & productCategory == obj1.category){
-            alert("borrando obj1")
-            delete obj1.name
-            delete obj1.category
-            delete obj1.price
+    else if (productName == watermelon.name & productCategory == watermelon.category) {
+        alert("borrando watermelon")
+        delete watermelon.name
+        delete watermelon.category
+        delete watermelon.price
     }
-    else{
+    else {
         alert("errorrrrr")
     }
 }
